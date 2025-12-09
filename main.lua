b683e5b36ec99d8a12f3b16f8337070ad8d3c8b8
breakcoree <breakcoreb@gmail.com>
2025-12-09 17:17:33 +0500

    empty

---
+++ main.lua
@@ -0,0 +1,16 @@
+--[[
+
+                                                                                                TODO:
+    - vote revealer
+    - calculate damage will taken from the bomb
+    - clantag
+    - indicators (all rage and legit tabs, slow ind)
+    - hitmarker
+    - trashtalk
+    - fake pitch
+    - yaw exploit yaw: -3.40282335e+30f
+    - fortnite damage
+    - anti afk
+    - e spam
+    - anti teamkill kick
+]]
